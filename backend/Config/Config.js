613ce62eeaf7effbe1db68cb7e4e2ff29553bbe2
Vimal-VR <vimalvr003@gmail.com>
2024-04-06 19:25:16 +0530

    config and ignore modified

--- backend/Config/Config.js
+++ backend/Config/Config.js
@@ -10,7 +10,7 @@ dotenv.config({path: `./env./env.${process.env.NODE_ENV}`});
 module.exports = {
 
   PORT: process.env.PORT || 8000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/User_Register',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://VimalRaj:ThalaVR003@cluster0.1b2f5.mongodb.net/ecommerce_demo_vr',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
 //   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
 //   accessKeyId: process.env.accessKeyId || 'accessKeyId',
--- backend/Server/Server.js
+++ backend/Server/Server.js
@@ -1,48 +1,43 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const Express=require('express');
-const  Mongoose  = require('mongoose');
-const cors = require ("cors");
-const config = require('../Config/Config')
+const bodyParser = require("body-parser");
+const Express = require("express");
+const Mongoose = require("mongoose");
+const cors = require("cors");
+const config = require("../Config/Config");
 // const Smtp =require('../Models/Smtp.model')
 
 var app = Express();
 
-const mongodbUrl = config.MONGODB_URL ;
+const mongodbUrl = config.MONGODB_URL;
 //create db
-Mongoose
-    .connect(mongodbUrl, { useNewUrlParser: true }, (error, database) => {
-        if (!error) {
-            //   throw error
-            console.log("success")
-        }
-        else {
-            console.log("error connection to the db")
-        }
-
-    });
+Mongoose.connect(mongodbUrl, { useNewUrlParser: true }, (error, database) => {
+  if (!error) {
+    //   throw error
+    console.log("Database connection success");
+  } else {
+    console.log("error connection to the db : ", error);
+  }
+});
 
 ///router connect
 
-const userRoute =require('../Routes/UserRoute/User.Route');
-const adminRoute = require('../Routes/AdminRoute/Admin.Route');
-const imageRoute = require('../Controllers/middleware/middleware');
+const userRoute = require("../Routes/UserRoute/User.Route");
+const adminRoute = require("../Routes/AdminRoute/Admin.Route");
+const imageRoute = require("../Controllers/middleware/middleware");
 // const imageRoute = require('../Routes/')
 
-    app.use(cors());
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({extended : true}));
-    app.use(express.static('public'));
-    
-    // const port = process.env.PORT || 8000
-    const port = config.PORT
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-    app.use('/User', userRoute);
-    app.use('/Admin', adminRoute);
-    app.use('/image', imageRoute);
-  
+// const port = process.env.PORT || 8000
+const port = config.PORT;
 
-    const server = app.listen(port, function () {
-        console.log('Server Lisening On Port : ' + port);
-    });
-    
+app.use("/User", userRoute);
+app.use("/Admin", adminRoute);
+app.use("/image", imageRoute);
+
+const server = app.listen(port, function () {
+  console.log("Server Lisening On Port : " + port);
+});
